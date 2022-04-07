f157d6740215a572e569d1cce6c50d79dbcb7692
Asif Akram <97173662+AKbabes@users.noreply.github.com>
2022-04-08 02:45:13 +0600

    update

---
+++ intellij/Practice/src/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+    public int num1;
+    public int num2;
+    public void addition(int a,int b){
+        int add=a+b;
+        System.out.println("Add = "+add);
+    }
+    public void substraction(int a,int b){
+        int sub=a-b;
+        System.out.println("Sub = "+sub);
+    }
+    public void multi(int a,int b){
+        int mul=a*b;
+        System.out.println("Mult = "+mul);
+    }
+    public void division(int a,int b){
+        int div=a/b;
+        System.out.println("Div = "+div);
+    }
+}
